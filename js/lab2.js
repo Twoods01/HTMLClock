3e4f46d1ff844c581d8b63f1ab4ccbab519b7aa2
Twoods01 <twoods0129@gmail.com>
2015-01-27 10:14:46 -0800

    Added weather to clock

--- js/lab2.js
+++ js/lab2.js
@@ -15,4 +15,33 @@ function doubleDigit(num){
     return num
 }
 
-setTimeout(function(){ getTime() }, 1000);
+function getTemp(){
+    $.getJSON("https://api.forecast.io/forecast/ce71be91edebab9e5b9962342dbc985c/35.300399,-120.662362?callback=?", 
+        function(data){
+            $("#forecastLabel").html(data.daily.summary);
+            $("#forecastIcon").attr("src", "img/" + data.daily.icon + ".png");
+
+            var temp = data.daily.data[0].temperatureMax;
+            var tempClass = ""
+
+                
+            if (temp >= 90)
+                tempClass = "hot";
+            else if (temp >= 80)
+                tempClass = "warm";
+            else if (temp >= 70)
+                tempClass = "nice";
+            else if (temp >= 60)
+                tempClass = "chilly";
+            else
+                tempClass = "cold";
+
+            $("body").addClass(tempClass)
+        })
+}
+
+$(document).ready(function(){
+    setTimeout(function(){ getTime() }, 1000);
+    getTemp();
+})
+    
