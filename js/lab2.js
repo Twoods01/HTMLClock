6dc16d25a21cd642c60d862078068665c9ea695f
Twoods01 <twoods0129@gmail.com>
2015-02-03 12:13:01 -0800

    Added local alarms

--- js/lab2.js
+++ js/lab2.js
@@ -3,7 +3,7 @@ function getTime(){
     var hours = doubleDigit(d.getHours());
     var minutes = doubleDigit(d.getMinutes());
     var seconds = doubleDigit(d.getSeconds());
-    document.getElementById("clock").innerHTML = hours + 
+    document.getElementById("clock").innerHTML = hours +
     ":" + minutes + ":" + seconds;
     setTimeout(function(){ getTime() }, 1000);
 }
@@ -16,7 +16,7 @@ function doubleDigit(num){
 }
 
 function getTemp(){
-    $.getJSON("https://api.forecast.io/forecast/ce71be91edebab9e5b9962342dbc985c/35.300399,-120.662362?callback=?", 
+    $.getJSON("https://api.forecast.io/forecast/ce71be91edebab9e5b9962342dbc985c/35.300399,-120.662362?callback=?",
         function(data){
             $("#forecastLabel").html(data.daily.summary);
             $("#forecastIcon").attr("src", "img/" + data.daily.icon + ".png");
@@ -24,7 +24,6 @@ function getTemp(){
             var temp = data.daily.data[0].temperatureMax;
             var tempClass = ""
 
-                
             if (temp >= 90)
                 tempClass = "hot";
             else if (temp >= 80)
@@ -40,8 +39,37 @@ function getTemp(){
         })
 }
 
+function hideAlarmPopup() {
+   $("#popup").addClass("hide");
+   $("#mask").addClass("hide");
+}
+
+function showAlarmPopup() {
+   $("#popup").removeClass("hide");
+   $("#mask").removeClass("hide");
+}
+
+function insertAlarm(hours, mins, ampm, name) {
+   newDiv = $("<div>");
+   newDiv.addClass("flexable");
+   nameDiv = $("<div>").addClass("name").html(name + "   ");
+   timeDiv = $("<div>").addClass("time padLeft").html(hours + ":" + mins + " " + ampm);
+   newDiv.append(nameDiv)
+   newDiv.append(timeDiv);
+   $("#alarms").append(newDiv);
+}
+
+function addAlarm() {
+   var hours = $("#hours option:selected").text();
+   var mins = $("#mins option:selected").text();
+   var ampm = $("#ampm option:selected").text();
+   var name = $("#alarmName").val();
+
+   insertAlarm(hours, mins, ampm, name);
+   hideAlarmPopup();
+}
+
 $(document).ready(function(){
     setTimeout(function(){ getTime() }, 1000);
     getTemp();
 })
-    
