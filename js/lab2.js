2d952134f0eb7a3530e0c59d982ef831e32a743e
Twoods01 <twoods0129@gmail.com>
2015-02-03 13:26:27 -0800

    Alarms now persistent with Parse

--- js/lab2.js
+++ js/lab2.js
@@ -44,18 +44,55 @@ function hideAlarmPopup() {
    $("#mask").addClass("hide");
 }
 
+function getAllAlarms() {
+
+   Parse.initialize("9wVKj7FtVb3xpTPXJLLowOd1JDxa32JX7TqIElLH", "LrilQx72ISPCSScfHhFWWZ1zXMWFxi5a9bfkb9P8");
+   var AlarmObject = Parse.Object.extend("Alarm");
+   var query = new Parse.Query(AlarmObject);
+   query.find({
+     success: function(results) {
+         for (var i = 0; i < results.length; i++) {
+            insertAlarm(results[i].attributes.hours, results[i].attributes.mins, results[i].attributes.ampm, results[i].attributes.alarmName, results[i].id);
+         }
+     }
+   });
+
+}
+
 function showAlarmPopup() {
    $("#popup").removeClass("hide");
    $("#mask").removeClass("hide");
 }
 
-function insertAlarm(hours, mins, ampm, name) {
-   newDiv = $("<div>");
-   newDiv.addClass("flexable");
+function deleteAlarm(){
+   var clickedButton = $(this);
+   var AlarmObject = Parse.Object.extend("Alarm");
+   var query = new Parse.Query(AlarmObject);
+
+   query.get(clickedButton.attr('id'), {
+
+     success: function(obj) {
+       //Remove the object from parse
+       obj.destroy({});
+       //Remove the div from HTML
+       clickedButton.parent("div").remove();
+     },
+
+     error: function(object, error) {
+        console.log(error)
+     }
+   });
+}
+
+function insertAlarm(hours, mins, ampm, name, id) {
+   newDiv = $("<div>").addClass("flexable");
    nameDiv = $("<div>").addClass("name").html(name + "   ");
    timeDiv = $("<div>").addClass("time padLeft").html(hours + ":" + mins + " " + ampm);
-   newDiv.append(nameDiv)
-   newDiv.append(timeDiv);
+
+   delButton = $("<input>").addClass("pullRight").attr("type", "button").attr("value", "Delete").attr("id", id);
+   delButton.on("click", deleteAlarm);
+
+   newDiv.append(nameDiv).append(timeDiv).append(delButton);
    $("#alarms").append(newDiv);
 }
 
@@ -65,11 +102,19 @@ function addAlarm() {
    var ampm = $("#ampm option:selected").text();
    var name = $("#alarmName").val();
 
-   insertAlarm(hours, mins, ampm, name);
-   hideAlarmPopup();
+
+   var AlarmObject = Parse.Object.extend("Alarm");
+   var alarmObject = new AlarmObject();
+   alarmObject.save({"hours": hours, "mins": mins, "ampm": ampm, "alarmName": name}, {
+   success: function(object) {
+      console.log(object)
+      insertAlarm(hours, mins, ampm, name, object.id);
+      hideAlarmPopup();
+   }
+   });
 }
 
 $(document).ready(function(){
-    setTimeout(function(){ getTime() }, 1000);
     getTemp();
+    getAllAlarms()
 })
